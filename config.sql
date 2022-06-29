e9e999987cd72f6d58423afebf0f5419eba39eb0
raafiinha <107967231+raafiinha@users.noreply.github.com>
2022-06-28 22:13:39 -0300

    Create config.sql

---
+++ config.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 28-Jun-2022 às 22:35
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cadastro de usuario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cadastro`
+--
+
+CREATE TABLE IF NOT EXISTS `conteudo` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(120) NOT NULL,
+  `ultimonome` longtext NOT NULL,
+  `telefone` varchar(30) NOT NULL,
+  `senha` int(30) NOT NULL,
+  `RG` int(30) NOT NULL,
+  `CPF` int(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Extraindo dados da tabela `conteudo`
+--
+
+INSERT INTO `cadastro de usuario` (`id`, `nome`, `ultimonome`, `telefone`, `senha_id`, `RG_id`, `CPF_id`) VALUES
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
