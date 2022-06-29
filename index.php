6026ef84fb06918534ce93353105f0ccc87e41ef
raafiinha <107967231+raafiinha@users.noreply.github.com>
2022-06-28 21:41:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-// Operação normal da API
+//Operação normal da API
 header( 'Acesso-Controle-Permitir-Origem: *' );
 header( 'Acesso-Controle-Permitir-Cabeçalhos: *' );
 header( 'Métodos de Controle de Acesso-Permitir: *' );
 header( 'Acesso-Controle-Permitir-Credenciais: true' );
 header( 'Tipo de conteúdo: json/aplicativo' );
 
-requer " connect.php ";
+requeira " connect.php ";
 requer " funções.php ";
 
 // Salva o método atual em uma variável
-$ método = $ _SERVER [ 'REQUEST_METHOD' ];
+$ método = $  _SERVER [ 'REQUEST_METHOD' ];
 
 // Divida a URL de solicitação em partes
-$ q = $ _GET [ 'q' ];
+$ q = $  _GET [ 'q' ];
 $ params = explodir( '/' , $ q );
 
 $ api = $ params [ 0 ];
@@ -23,36 +23,36 @@ $ codigo = $ params [ 2 ];
 
 
 /* Além disso, tudo é muito simples: verificamos a solicitação correspondente
-	e chame a função desejada. Todas as funções estão descritas no arquivo functions.php. */
+	e chame a função desejada. Todas as funções estão no arquivo functions.php. */
 if ( $ método === 'POST' ) {
-	if ( $ type === 'registrar' ) {
-		addUser( $ connect , $ _POST );
+	if ( $ tipo === 'registrador' ) {
+		addUser( $ connect , $  _POST );
 	} elseif ( $ type === 'login' ) {
-		login( $ conectar , $ _POST );
+		login( $ conectar , $  _POST );
 	
-			// Aqui a mágica é feita abaixo para que possamos obter corretamente os dados de "body" - "raw" (estamos falando do Postman, já que a Api foi testada nele)
+			// Aqui a mágica é para que obteve corretamente os dados de "body" - "raw" (estamos falando do Postman, já que a abaixopi foi nele)
 
 			$ data = file_get_contents( 'php://input' ); // Obtém dados em JSON
-			$ data = json_decode( $ data , true ); //Decodifica do JSON para passar a variável como parâmetro
-			booking( $ connect , $ data );	// Tudo está claro aqui, chamamos a função e passamos os dados para ela como o 2º parâmetro, que recebemos acima.
+			$ data = json_decode( $ data , true ); //Decodifica do JSON para passar uma variável como parâmetro
+			booking( $ connect , $ data );	// Tudo está claro como acima.
 	}
 }
 
 if ( $ método === 'GET' ) {
 	if ( $ tipo === 'documento' ) {
-		RG( $ connect , $ _GET [ 'consulta' ]);
+		RG ( $ conectar , $  _GET [ 'consulta' ]);
 	} elseif ( $ type === 'RG' ) {
-		NUMERO( $ connect , $ _GET );
+		CPF ( $ conectar , $  _GET );
 	} elseif ( $ type === 'NUMERO' && isset( $ code )) {
 		armorInfo( $ connect , $ code );
 	} elseif ( $ api === 'usuário' && $ type === 'NUMERO' ) {
-		// Aqui nós pegamos o Bearer e o passamos para a função com um check.
-		$ cabeçalhos = apache_request_headers();
+		// Aqui nós pegamos o Bearer e a identificação para a função com um check.
+		$ cabeçalhos = apache_request_headers() ;
 		$ token = $ headers [ 'Autorização' ];
 		myBrone( $ connect , $ token );
 	} elseif ( $ api === 'usuário' ) {
 		// Aqui também obtemos o token do portador.
-		$ cabeçalhos = apache_request_headers();
+		$ cabeçalhos = apache_request_headers() ;
 		$ token = $ headers [ 'Autorização' ];
 		infoUser( $ connect , $ token );
 	}
